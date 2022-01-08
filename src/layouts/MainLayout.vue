d1801bf75fbace3470bed4dc01d623d069fe2094
Natalia <nataliabiscaro.18@gmail.com>
2022-01-07 23:47:32 -0300

    Quasar Getting Started Guide

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-layout view="hHh Lpr lFf">
+    <q-header>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <q-btn flat rounded icon="mdi-logout"></q-btn>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-toolbar class="bg-primary"/>
+        <q-item class = "bg-grey-3">
+            <q-item-section>
+              Lists
+            </q-item-section>
+              <q-item-section side>
+              <q-btn icon="mdi-plus" flat round size=sm />
+              </q-item-section>
+        </q-item>
+        <q-item clickable>
+            <q-item-section side>
+              <q-icon name="mdi-shopping"/>
+            </q-item-section>
+
+        <q-item-section >
+          Shopping List
+        </q-item-section >
+        <q-item-section side >
+            12
+        </q-item-section >
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,47 @@
+<template>
+
+  <q-page class="row">
+
+    <q-card class="col-xs-9">
+      <q-toolbar>
+        <q-input dense placeholder="Search" borderless class="full-width" :model-value="search" >
+          <template #append> <q-icon name = "mdi-magnify"/></template>
+        </q-input>
+      </q-toolbar>
+          <q-list bordered>
+            <q-item>
+              <q-item-section side>
+                <q-checkbox :model-value="true"></q-checkbox>
+              </q-item-section>
+                <q-item-section> Go Shopping </q-item-section>
+                <q-item-section side>
+                  <q-btn size="sm" flat round icon="mdi-delete"></q-btn>
+                </q-item-section>
+            </q-item>
+          </q-list>
+        </q-card>
+        <div class='col-xs-3'>
+          <q-toolbar class="bg-primary">
+            <q-btn
+              fab style="margin-bottom: -45px;"
+              class="q-ml-md"
+              color = 'green-4'
+              icon="mdi-plus"/>
+          </q-toolbar>
+        </div>
+    </q-page>
+
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'PageIndex',
+
+  data () {
+    return {
+      selected: null
+    }
+  }
+})
+</script >
